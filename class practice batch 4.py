f9f04672ef8dcd6cb664f77fd1f5dc952c2d5b08
areebababar1145-png <areebababar1145@gmail.com>
2025-08-17 11:55:27 +0500

    Create class practice batch 4.py

---
+++ class practice batch 4.py
@@ -0,0 +1,15 @@
+list1=["ai","batch4",2024,3.25,19,4.2]
+print(list1)
+print(list1[1])
+for char in list1:
+    print(char)
+list1.append("month") 
+print (list1 )
+list1.remove(2024)
+print(list1)
+list1.pop(1)
+print(list1)
+list1[2]=500 # its interchange the valvue at the index 2
+print(list1)
+print(type(list1))
+print(type(list1[1]))
